f3448a5ebce9637398b3345f49ca80a41f2fea25
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-05-31 08:31:35 +0300

    Models

---
+++ app/models.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db, login
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    properties = db.relationship('Property', backref='owner', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+class Property(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140))
+    description = db.Column(db.Text)
+    price = db.Column(db.Numeric(10, 2))
+    location = db.Column(db.String(140))
+    date_listed = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    property_id = db.Column(db.Integer, db.ForeignKey('property.id'))
+    date_booked = db.Column(db.DateTime, default=datetime.utcnow)
+    status = db.Column(db.String(64), default='Pending')
