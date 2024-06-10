ec27d4df1fc58a09b3cf3680fc5d25aa2448a8bd
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-06-10 09:36:03 +0300

    Landing

---
+++ elcent_app/index.php
@@ -0,0 +1,11 @@
+<?php include('includes/header.php'); ?>
+<div class="container mt-5">
+  <div class="jumbotron">
+    <h1 class="display-4">Welcome to Elcent Realtors</h1>
+    <p class="lead">We offer access to your dream property in the most convenient and cost-effective manner.</p>
+    <hr class="my-4">
+    <p>Explore our properties and find your perfect home today.</p>
+    <a class="btn btn-primary btn-lg" href="/pages/properties.php" role="button">View Properties</a>
+  </div>
+</div>
+<?php include('includes/footer.php'); ?>
