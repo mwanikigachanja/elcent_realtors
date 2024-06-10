ac12b008113fd21c4db63675536025147e3f9cd8
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-06-10 09:35:35 +0300

    Includes

---
+++ elcent_app/includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer mt-auto py-3 bg-light">
+    <div class="container">
+      <span class="text-muted">© 2023 Elcent Realtors. All rights reserved.</span>
+    </div>
+  </footer>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
