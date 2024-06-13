8f0d28aa3a4f164e2eb5d282bb7b9f096906aa10
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-06-13 09:52:49 +0300

    Adminadd

--- realtors/admin/register_admin.php
+++ realtors/admin/register_admin.php
@@ -19,6 +19,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html>
 <head>
     <title>Register Admin</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
     <h2>Register Admin</h2>
