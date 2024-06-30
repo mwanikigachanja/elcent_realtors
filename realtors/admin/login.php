a404068a7345b619b315f9b03461491ca1d1f948
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-06-30 19:42:50 +0300

    Content

--- realtors/admin/login.php
+++ realtors/admin/login.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 session_start();
-require_once "config.php";
+require_once "../includes/functions.php";
 
 $username = $password = "";
 $username_err = $password_err = "";
