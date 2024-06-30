7e78ada61d8f19f4139b9571fd6d2028d67f855e
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-06-30 20:31:28 +0300

    Speed boost

--- realtors/admin/index.php
+++ realtors/admin/index.php
@@ -2,7 +2,7 @@
 // Initialize the session
 session_start();
 
-// Check if the user is logged in, if not then redirect to login page
+// Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
@@ -11,127 +11,125 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 // Include config file
 require_once "config.php";
 
-// Get counts for the dashboard
-$result = mysqli_query($link, "SELECT COUNT(*) as count FROM properties");
-$property_count = mysqli_fetch_assoc($result)['count'];
-
-$result = mysqli_query($link, "SELECT COUNT(*) as count FROM testimonials");
-$testimonial_count = mysqli_fetch_assoc($result)['count'];
-
-$result = mysqli_query($link, "SELECT COUNT(*) as count FROM blogs");
-$blog_count = mysqli_fetch_assoc($result)['count'];
-
-$result = mysqli_query($link, "SELECT COUNT(*) as count FROM users");
-$user_count = mysqli_fetch_assoc($result)['count'];
+// Fetch data for dashboard
+$property_count = mysqli_query($link, "SELECT COUNT(*) AS count FROM properties")->fetch_assoc()['count'];
+$testimonial_count = mysqli_query($link, "SELECT COUNT(*) AS count FROM testimonials")->fetch_assoc()['count'];
+$blog_count = mysqli_query($link, "SELECT COUNT(*) AS count FROM blogs")->fetch_assoc()['count'];
+$user_count = mysqli_query($link, "SELECT COUNT(*) AS count FROM users")->fetch_assoc()['count'];
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Admin Dashboard</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="path/to/ionicons/css/ionicons.min.css">
-    <link rel="stylesheet" href="styles.css">
+    <title>Admin Dashboard - Elcent Realtors</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
-    <div class="container-fluid">
-        <div class="row">
-            <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-                <div class="sidebar-sticky">
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">
-                                <span class="ion-ios-speedometer"></span>
-                                Dashboard
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="manage_properties.php">
-                                <span class="ion-ios-home"></span>
-                                Manage Properties
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="manage_testimonials.php">
-                                <span class="ion-ios-people"></span>
-                                Manage Testimonials
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="manage_blogs.php">
-                                <span class="ion-ios-paper"></span>
-                                Manage Blogs
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="manage_users.php">
-                                <span class="ion-ios-person"></span>
-                                Manage Users
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="logout.php">
-                                <span class="ion-ios-log-out"></span>
-                                Logout
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+    <div class="d-flex" id="wrapper">
+        <!-- Sidebar -->
+        <div class="bg-light border-right" id="sidebar-wrapper">
+            <div class="sidebar-heading">Elcent Realtors </div>
+            <div class="list-group list-group-flush">
+                <a href="index.php" class="list-group-item list-group-item-action bg-light">Dashboard</a>
+                <a href="properties.php" class="list-group-item list-group-item-action bg-light">Manage Properties</a>
+                <a href="testimonials.php" class="list-group-item list-group-item-action bg-light">Manage Testimonials</a>
+                <a href="blogs.php" class="list-group-item list-group-item-action bg-light">Manage Blogs</a>
+                <a href="users.php" class="list-group-item list-group-item-action bg-light">Manage Users</a>
+                <a href="logout.php" class="list-group-item list-group-item-action bg-light">Logout</a>
+            </div>
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+                <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
             </nav>
 
-            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                    <h1 class="h2">Dashboard</h1>
-                </div>
-
+            <div class="container-fluid">
+                <h1 class="mt-4">Admin Dashboard</h1>
                 <div class="row">
-                    <div class="col-md-3">
-                        <div class="card text-white bg-primary mb-3">
-                            <div class="card-header">Properties</div>
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
                             <div class="card-body">
-                                <h5 class="card-title"><?php echo $property_count; ?></h5>
-                                <p class="card-text">Total Properties</p>
+                                <div class="d-flex no-block">
+                                    <div class="m-r-20 align-self-center"><i class="fas fa-home fa-2x text-primary"></i></div>
+                                    <div class="align-self-center">
+                                        <h6 class="text-muted m-t-10 m-b-0">Properties</h6>
+                                        <h2 class="m-t-0"><?php echo $property_count; ?></h2>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-3">
-                        <div class="card text-white bg-success mb-3">
-                            <div class="card-header">Testimonials</div>
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
                             <div class="card-body">
-                                <h5 class="card-title"><?php echo $testimonial_count; ?></h5>
-                                <p class="card-text">Total Testimonials</p>
+                                <div class="d-flex no-block">
+                                    <div class="m-r-20 align-self-center"><i class="fas fa-comment fa-2x text-primary"></i></div>
+                                    <div class="align-self-center">
+                                        <h6 class="text-muted m-t-10 m-b-0">Testimonials</h6>
+                                        <h2 class="m-t-0"><?php echo $testimonial_count; ?></h2>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-3">
-                        <div class="card text-white bg-warning mb-3">
-                            <div class="card-header">Blogs</div>
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
                             <div class="card-body">
-                                <h5 class="card-title"><?php echo $blog_count; ?></h5>
-                                <p class="card-text">Total Blogs</p>
+                                <div class="d-flex no-block">
+                                    <div class="m-r-20 align-self-center"><i class="fas fa-newspaper fa-2x text-primary"></i></div>
+                                    <div class="align-self-center">
+                                        <h6 class="text-muted m-t-10 m-b-0">Blogs</h6>
+                                        <h2 class="m-t-0"><?php echo $blog_count; ?></h2>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-3">
-                        <div class="card text-white bg-danger mb-3">
-                            <div class="card-header">Users</div>
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
                             <div class="card-body">
-                                <h5 class="card-title"><?php echo $user_count; ?></h5>
-                                <p class="card-text">Total Users</p>
+                                <div class="d-flex no-block">
+                                    <div class="m-r-20 align-self-center"><i class="fas fa-users fa-2x text-primary"></i></div>
+                                    <div class="align-self-center">
+                                        <h6 class="text-muted m-t-10 m-b-0">Users</h6>
+                                        <h2 class="m-t-0"><?php echo $user_count; ?></h2>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <!-- Add more detailed analytics and management sections here -->
-                <!-- Example: Recent Properties, Latest Testimonials, etc. -->
-            </main>
+                <!-- Add more content here for forms to add properties, testimonials, blogs, etc. -->
+            </div>
         </div>
+        <!-- /#page-content-wrapper -->
     </div>
+    <!-- /#wrapper -->
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
 </body>
 </html>
---
+++ realtors/admin/index_02.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+// Check if the user is logged in, if not then redirect to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
+// Debugging - print session variables
+print_r($_SESSION);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="page-header">
+        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+    </div>
+    <p>
+        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+    </p>
+</body>
+</html>
