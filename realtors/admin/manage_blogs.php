e41acaf9855ee5f85f11a8d12a0cdc5e3d4f6a67
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-07-01 14:41:55 +0300

    Blogs admin

--- realtors/admin/manage_blogs.php
+++ realtors/admin/manage_blogs.php
@@ -1,60 +1,38 @@
 <?php
-include('config.php');
-include('functions.php');
+include 'header.php';
+include 'config.php';
 
-// Fetch blogs
-$blogs = mysqli_query($link, "SELECT * FROM blogs");
-
-if (isset($_POST['delete_blog'])) {
-    $id = $_POST['id'];
-    mysqli_query($link, "DELETE FROM blogs WHERE id='$id'");
-    header('Location: manage_blogs.php');
-}
-
-if (isset($_POST['edit_blog'])) {
-    $id = $_POST['id'];
-    $title = $_POST['title'];
-    $content = $_POST['content'];
-    // Additional fields as per your DB structure
-    mysqli_query($link, "UPDATE blogs SET title='$title', content='$content' WHERE id='$id'");
-    header('Location: manage_blogs.php');
-}
+// Fetch blogs from database
+$query = "SELECT * FROM blogs";
+$result = mysqli_query($link, $query);
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Manage Blogs</title>
-    <!-- Include your CSS here -->
-</head>
-<body>
-    <h1>Manage Blogs</h1>
-    <table>
-        <thead>
+<h2>Manage Blogs</h2>
+<a href="add_blog.php" class="btn btn-success mb-3">Add New Blog</a>
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Status</th>
+            <th>Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php while ($row = mysqli_fetch_assoc($result)) { ?>
             <tr>
-                <th>ID</th>
-                <th>Title</th>
-                <th>Content</th>
-                <th>Actions</th>
+                <td><?php echo $row['id']; ?></td>
+                <td><?php echo $row['title']; ?></td>
+                <td><?php echo $row['author']; ?></td>
+                <td><?php echo $row['status']; ?></td>
+                <td>
+                    <a href="edit_blog.php?id=<?php echo $row['id']; ?>" class="btn btn-primary">Edit</a>
+                    <a href="delete_blog.php?id=<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a>
+                </td>
             </tr>
-        </thead>
-        <tbody>
-            <?php while($row = mysqli_fetch_assoc($blogs)): ?>
-                <tr>
-                    <td><?php echo $row['id']; ?></td>
-                    <td><?php echo $row['title']; ?></td>
-                    <td><?php echo $row['content']; ?></td>
-                    <td>
-                        <form method="post">
-                            <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
-                            <input type="submit" name="edit_blog" value="Edit">
-                            <input type="submit" name="delete_blog" value="Delete">
-                        </form>
-                    </td>
-                </tr>
-            <?php endwhile; ?>
-        </tbody>
-    </table>
-</body>
-</html>
+        <?php } ?>
+    </tbody>
+</table>
+<?php
+include 'footer.php';
+?>
