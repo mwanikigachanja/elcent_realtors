5e56593b28810964b667e5025b754cf925b2df51
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-07-01 14:41:32 +0300

    property manager

--- realtors/admin/manage_properties.php
+++ realtors/admin/manage_properties.php
@@ -1,63 +1,38 @@
 <?php
-include('config.php');
-include('functions.php');
+include 'header.php';
+include 'config.php';
 
-// Fetch properties
-$properties = mysqli_query($link, "SELECT * FROM properties");
-
-if (isset($_POST['delete_property'])) {
-    $id = $_POST['id'];
-    mysqli_query($link, "DELETE FROM properties WHERE id='$id'");
-    header('Location: manage_properties.php');
-}
-
-if (isset($_POST['edit_property'])) {
-    $id = $_POST['id'];
-    $name = $_POST['name'];
-    $location = $_POST['location'];
-    $price = $_POST['price'];
-    // Additional fields as per your DB structure
-    mysqli_query($link, "UPDATE properties SET name='$name', location='$location', price='$price' WHERE id='$id'");
-    header('Location: manage_properties.php');
-}
+// Fetch properties from database
+$query = "SELECT * FROM properties";
+$result = mysqli_query($link, $query);
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Manage Properties</title>
-    <!-- Include your CSS here -->
-</head>
-<body>
-    <h1>Manage Properties</h1>
-    <table>
-        <thead>
+<h2>Manage Properties</h2>
+<a href="add_property.php" class="btn btn-success mb-3">Add New Property</a>
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Price</th>
+            <th>Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php while ($row = mysqli_fetch_assoc($result)) { ?>
             <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Location</th>
-                <th>Price</th>
-                <th>Actions</th>
+                <td><?php echo $row['id']; ?></td>
+                <td><?php echo $row['title']; ?></td>
+                <td><?php echo $row['description']; ?></td>
+                <td><?php echo $row['price']; ?></td>
+                <td>
+                    <a href="edit_property.php?id=<?php echo $row['id']; ?>" class="btn btn-primary">Edit</a>
+                    <a href="delete_property.php?id=<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a>
+                </td>
             </tr>
-        </thead>
-        <tbody>
-            <?php while($row = mysqli_fetch_assoc($properties)): ?>
-                <tr>
-                    <td><?php echo $row['id']; ?></td>
-                    <td><?php echo $row['name']; ?></td>
-                    <td><?php echo $row['location']; ?></td>
-                    <td><?php echo $row['price']; ?></td>
-                    <td>
-                        <form method="post">
-                            <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
-                            <input type="submit" name="edit_property" value="Edit">
-                            <input type="submit" name="delete_property" value="Delete">
-                        </form>
-                    </td>
-                </tr>
-            <?php endwhile; ?>
-        </tbody>
-    </table>
-</body>
-</html>
+        <?php } ?>
+    </tbody>
+</table>
+<?php
+include 'footer.php';
+?>
