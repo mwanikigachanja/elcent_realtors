bd1323313d1c04425062b2cd78c31d4409682c16
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-07-01 14:42:57 +0300

    User manager

--- realtors/admin/manage_users.php
+++ realtors/admin/manage_users.php
@@ -1,60 +1,38 @@
 <?php
-include('config.php');
-include('functions.php');
+include 'header.php';
+include 'config.php';
 
-// Fetch users
-$users = mysqli_query($link, "SELECT * FROM users");
-
-if (isset($_POST['delete_user'])) {
-    $id = $_POST['id'];
-    mysqli_query($link, "DELETE FROM users WHERE id='$id'");
-    header('Location: manage_users.php');
-}
-
-if (isset($_POST['edit_user'])) {
-    $id = $_POST['id'];
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    // Additional fields as per your DB structure
-    mysqli_query($link, "UPDATE users SET username='$username', email='$email' WHERE id='$id'");
-    header('Location: manage_users.php');
-}
+// Fetch users from database
+$query = "SELECT * FROM users";
+$result = mysqli_query($link, $query);
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Manage Users</title>
-    <!-- Include your CSS here -->
-</head>
-<body>
-    <h1>Manage Users</h1>
-    <table>
-        <thead>
+<h2>Manage Users</h2>
+<a href="add_user.php" class="btn btn-success mb-3">Add New User</a>
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php while ($row = mysqli_fetch_assoc($result)) { ?>
             <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Email</th>
-                <th>Actions</th>
+                <td><?php echo $row['id']; ?></td>
+                <td><?php echo $row['username']; ?></td>
+                <td><?php echo $row['email']; ?></td>
+                <td><?php echo $row['role']; ?></td>
+                <td>
+                    <a href="edit_user.php?id=<?php echo $row['id']; ?>" class="btn btn-primary">Edit</a>
+                    <a href="delete_user.php?id=<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a>
+                </td>
             </tr>
-        </thead>
-        <tbody>
-            <?php while($row = mysqli_fetch_assoc($users)): ?>
-                <tr>
-                    <td><?php echo $row['id']; ?></td>
-                    <td><?php echo $row['username']; ?></td>
-                    <td><?php echo $row['email']; ?></td>
-                    <td>
-                        <form method="post">
-                            <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
-                            <input type="submit" name="edit_user" value="Edit">
-                            <input type="submit" name="delete_user" value="Delete">
-                        </form>
-                    </td>
-                </tr>
-            <?php endwhile; ?>
-        </tbody>
-    </table>
-</body>
-</html>
+        <?php } ?>
+    </tbody>
+</table>
+<?php
+include 'footer.php';
+?>
