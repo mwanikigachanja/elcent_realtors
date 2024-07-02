df7849bb5d9a866e9efba02bc68fe165c4cdf2d3
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-07-02 20:22:10 +0300

    Manage property

--- realtors/admin/manage_properties.php
+++ realtors/admin/manage_properties.php
@@ -26,6 +26,31 @@
     </style>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="index.php">Elcent Realtors Admin</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="manage_properties.php">Manage Properties</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="manage_blogs.php">Manage Blogs</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="manage_testimonials.php">Manage Testimonials</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="manage_users.php">Manage Users</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="logout.php">Logout</a>
+      </li>
+    </ul>
+  </div>
+</nav>
     <div class="container">
         <h1 class="mb-4">Manage Properties</h1>
         <button type="button" class="btn btn-custom" data-toggle="modal" data-target="#addPropertyModal">
