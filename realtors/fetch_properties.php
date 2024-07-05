9c52d10105171b360cf5bc5714e0136761ca1a4d
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-07-05 08:49:27 +0300

    Fetch array prop

---
+++ realtors/fetch_properties.php
@@ -0,0 +1,13 @@
+<?php
+include 'config.php';
+
+$query = "SELECT * FROM properties";
+$result = mysqli_query($conn, $query);
+
+$properties = array();
+while($row = mysqli_fetch_assoc($result)) {
+    $properties[] = $row;
+}
+
+echo json_encode($properties);
+?>
