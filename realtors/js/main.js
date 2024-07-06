bcd6fe3007df887945e5ac014cf1ab26ee7d8bf9
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-07-06 18:58:38 +0300

    back top JS

--- realtors/js/main.js
+++ realtors/js/main.js
@@ -341,4 +341,20 @@ $(document).ready(function(){
     });
 });
 
-  
+// Back to Top Button
+document.addEventListener("DOMContentLoaded", function() {
+  var backToTopButton = document.getElementById("back-to-top");
+
+  window.onscroll = function() {
+      if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+          backToTopButton.style.display = "block";
+      } else {
+          backToTopButton.style.display = "none";
+      }
+  };
+
+  backToTopButton.onclick = function(e) {
+      e.preventDefault();
+      window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+});
