1482a0d840e13a623206228f94103ab277d7da9a
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-07-14 21:04:11 +0300

    Latest

--- realtors/index_prep.php
+++ realtors/index_prep.php
@@ -295,4 +295,95 @@ Here is the updated `index.php`:
                 }
                 ?>
             </div>
-       
+        </div>
+    </section>
+
+    <!-- Testimonials Section -->
+    <section id="testimonials" class="py-5 bg-light">
+        <div class="container">
+            <h2 class="text-center mb-4">Testimonials</h2>
+            <div class="row">
+                <?php
+                // Fetch testimonials from database
+                $query = "SELECT name, image, testimonial FROM testimonials";
+                $result = mysqli_query($link, $query);
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<div class='col-md-4 mb-4'>
+                            <div class='card'>
+                                <img src='images/{$row['image']}' alt='{$row['name']}' class='card-img-top'>
+                                <div class='card-body'>
+                                    <h5 class='card-title'>{$row['name']}</h5>
+                                    <p class='card-text'>{$row['testimonial']}</p>
+                                </div>
+                            </div>
+                          </div>";
+                }
+                ?>
+            </div>
+        </div>
+    </section>
+
+    <!-- Blog Section -->
+    <section id="blog" class="py-5">
+        <div class="container">
+            <h2 class="text-center mb-4">Latest Blog Posts</h2>
+            <div class="row">
+                <?php
+                // Fetch blog posts from database
+                $query = "SELECT title, image, excerpt FROM blog_posts";
+                $result = mysqli_query($link, $query);
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<div class='col-md-4 mb-4'>
+                            <div class='card'>
+                                <img src='images/{$row['image']}' alt='{$row['title']}' class='card-img-top'>
+                                <div class='card-body'>
+                                    <h5 class='card-title'>{$row['title']}</h5>
+                                    <p class='card-text'>{$row['excerpt']}</p>
+                                    <a href='blog_details.php?id={$row['id']}' class='btn btn-primary'>Read More</a>
+                                </div>
+                            </div>
+                          </div>";
+                }
+                ?>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="bg-dark text-white py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h5>Elcent Realtors</h5>
+                    <p>Contact us for the best property deals in Kenya.</p>
+                    <ul class="list-unstyled">
+                        <li><i class="fas fa-phone"></i> +254 717 388544</li>
+                        <li><i class="fas fa-envelope"></i> elcentrealtors@gmail.com</li>
+                        <li><i class="fas fa-map-marker-alt"></i> Ebby Towers, 2nd Floor Room D5, Kenyatta Avenue, Kitale</li>
+                    </ul>
+                </div>
+                <div class="col-md-6 text-right">
+                    <h5>Follow Us</h5>
+                    <ul class="list-inline">
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="text-center mt-3">
+                <p>&copy; 2024 Elcent Realtors. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Back to Top Button -->
+    <a href="#" class="back-to-top"><i class="fas fa-arrow-up"></i></a>
+
+    <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
