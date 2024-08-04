3ac899cc4e2170159a91a8d556b3141e121695ce
Charles Mwaniki <mwanikigachanja@gmail.com>
2024-08-04 20:07:07 +0300

    style done

--- realtors/index.php
+++ realtors/index.php
@@ -302,6 +302,7 @@ header a:hover, footer a:hover {
     <a href="#" class="back-to-top"><i class="fas fa-arrow-up"></i></a>
 
     <!-- Scripts -->
+    <script src="js/carousel.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
